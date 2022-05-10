01d6cc01dfcb39ad9402507fc60c2a2eb087549f
Xavin <xavin@hydra.bot>
2022-05-10 15:17:37 +0200

    chore: remove unused subprojects config

--- services/build.gradle.kts
+++ services/build.gradle.kts
@@ -3,18 +3,4 @@ plugins {
 }
 
 subprojects {
-    //jib {
-    //from {
-    //image = "openjdk@sha256:f9be8e89a2bbf973dcd6c286f85bb0f68a8f9d5fa7c6241eb59f07add4a24789"
-    //}
-
-    //to {
-    //image = "ghcr.io/soundcloud/$name"
-    //tags = setOf(System.getenv("SEMAPHORE_GIT_SHA"), "latest")
-    //auth {
-    //username = System.getenv("GITHUB_ACTOR")
-    //password = System.getenv("GITHUB_TOKEN")
-    //}
-    //}
-    //}
 }
