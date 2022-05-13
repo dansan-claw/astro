98a07c4c9dace130c07ed53e26dc35fdebd13f99
Xavin <xavin@hydra.bot>
2022-05-13 14:09:36 +0200

    feat: add shared webClient config

---
+++ shared/core/src/main/kotlin/space/astro/shared/core/configs/WebClientConfig.kt
@@ -0,0 +1,15 @@
+package space.astro.shared.core.configs
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties("web.client")
+class WebClientConfig {
+
+    var httpMaxConnections = 100
+    var httpMaxIdleTime: Long = 20
+    var httpMaxLifeTime: Long = 60
+    var httpPendingAcquireTimeout: Long = 20
+    var httpEvictInBackground: Long = 120
+}
