b529d874ca52507747c19d375dfeb9197060dfca
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-08-16 02:54:11 +0200

    chore: gradle version + gradle versions catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,21 @@
 group = "space.astro"
 
+// Needed for version catalogs with gradle versions before 8.1.0
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     base
     java
 
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    alias(libs.plugins.jvm)
+    alias(libs.plugins.spring)
+    alias(libs.plugins.serialization)
 }
 
 repositories {
     mavenCentral()
 }
 
+// TODO: Migrate this to buildSrc or conventional builds when version catalogs are supported
 subprojects {
     repositories {
         mavenCentral()
