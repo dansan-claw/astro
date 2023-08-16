6146b69d72d76f3484e3a3fd58bcd38f12e53d47
Giuliopime <60524738+Giuliopime@users.noreply.github.com>
2023-08-16 03:09:39 +0200

    chore: better comments

--- shared/core/src/main/kotlin/space/astro/shared/core/io/caching/redis/RedisKey.kt
+++ shared/core/src/main/kotlin/space/astro/shared/core/io/caching/redis/RedisKey.kt
@@ -2,7 +2,18 @@ package space.astro.shared.core.io.caching.redis
 
 enum class RedisKey(val key: String) {
 
-    DISCORD_USER_CREDENTIALS("DUC:%s"), // USER_ID,
-    WEB_SESSION_TOKEN("WST:%s:%s"), // DISCORD_USER_ID:SESSION_TOKEN
-    WEB_SESSION_TOKENS("WST:%s:*") // DISCORD_USER_ID
+    /**
+     * Format with: USER_ID
+     */
+    DISCORD_USER_CREDENTIALS("DUC:%s"),
+
+    /**
+     * Format with: DISCORD_USER_ID:SESSION_TOKEN
+     */
+    WEB_SESSION_TOKEN("WST:%s:%s"),
+
+    /**
+     * Format with: DISCORD_USER_ID
+     */
+    WEB_SESSION_TOKENS("WST:%s:*")
 }
