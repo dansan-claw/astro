9f66e4f052033c07b3525ed9a92c764e43300ae2
Xavin <xavin@hydra.bot>
2024-02-10 18:10:14 +0100

    debug: add JDA version log

--- services/bot/src/main/kotlin/space/astro/bot/events/listeners/ReadyEventListener.kt
+++ services/bot/src/main/kotlin/space/astro/bot/events/listeners/ReadyEventListener.kt
@@ -1,12 +1,13 @@
 package space.astro.bot.events.listeners
 
 import mu.KotlinLogging
+import net.dv8tion.jda.api.JDAInfo
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import space.astro.bot.interactions.handlers.command.CommandHandler
 
-private val log = KotlinLogging.logger {  }
+private val log = KotlinLogging.logger { }
 
 @Component
 class ReadyEventListener(
@@ -16,6 +17,7 @@ class ReadyEventListener(
     @EventListener
     fun receiveReadyEvent(event: ReadyEvent) {
         log.info("Logged in as ${event.jda.selfUser.name}")
+        log.info { "Launched JDA with Version: ${JDAInfo.VERSION}" }
 
         val shardId = event.jda.shardInfo.shardId
 
