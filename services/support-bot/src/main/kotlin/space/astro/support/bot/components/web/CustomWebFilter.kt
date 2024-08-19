ab62e227f5e462923e91ba63313f735abf215b03
Giulio Pimenoff Verdolin <60524738+Giuliopime@users.noreply.github.com>
2024-08-19 21:38:52 +0200

    fix: support bot auth

--- services/support-bot/src/main/kotlin/space/astro/support/bot/components/web/CustomWebFilter.kt
+++ services/support-bot/src/main/kotlin/space/astro/support/bot/components/web/CustomWebFilter.kt
@@ -9,16 +9,19 @@ import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
+import space.astro.shared.core.configs.KubeConfig
 import space.astro.shared.core.configs.SupportBotApiConfig
 
 @Component
 class CustomWebFilter(
-    private val supportBotApiConfig: SupportBotApiConfig
+    private val supportBotApiConfig: SupportBotApiConfig,
+    private val kubeConfig: KubeConfig
 ) : WebFilter {
 
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         val request = exchange.request
         val response = exchange.response
+        val requestPath = request.path.toString()
 
         response.headers.set("Access-Control-Allow-Origin", supportBotApiConfig.originUrl)
         response.headers.set("Access-Control-Allow-Methods", "*")
@@ -27,6 +30,23 @@ class CustomWebFilter(
         // chain preflight requests
         if (request.method == HttpMethod.OPTIONS) {
             return chain.filter(exchange)
+        }
+
+        if (requestPath.startsWith("/ready") || requestPath.startsWith("/shutdown")) {
+            return mono {
+                val authHeader = request.headers["Authorization"]?.get(0)
+                    ?: run {
+                        response.statusCode = HttpStatus.UNAUTHORIZED
+                        return@mono null
+                    }
+
+                if (authHeader != kubeConfig.lifecycleAuthorization) {
+                    response.statusCode = HttpStatus.UNAUTHORIZED
+                    return@mono null
+                }
+
+                chain.filter(exchange).awaitSingleOrNull()
+            }
         }
 
         return mono {
